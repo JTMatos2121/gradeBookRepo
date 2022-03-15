2ae7c74b704b34eec1c69f83250c42e49ca94e3b
JTMatos2121 <86131140+JTMatos2121@users.noreply.github.com>
2022-03-14 23:00:54 -0500

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,72 +1,59 @@
+public class Gradebook
+{
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
-//      Initially the program was made to propmtforInteger and asigning them to a variable
-////    Then i realized that I was not using the getters and setters 
-//      for that reason i have completly changed the program approach 
-//      I will continue to work on the program but i dont want to lose more points per date been late
-        int numQuizzes = -0;
-        int numStudents = 0;
-        double scores = 0;
-        
+//        After collecting my thoughts I used the getters and setters
+//        used methods for int student and quizzes with input validation 
         System.out.println("Welcome to Simple Gradebook v2.0");
-//      The goal is to use the setters and getters but I fail to visualize the process 
-        GradeBook r = new GradeBook();
-        
-        
-        
-        
-        
-        r.getStudents();promptForInteger("Enter the number of students: ",GradeBook.MIN_STUDENTS,GradeBook.MAX_STUDENTS);
-        
-//        r.setStudents(numStudents);promptForInteger("Enter the number of students: ",GradeBook.MIN_STUDENTS,GradeBook.MAX_STUDENTS);
-//        r.setQuizzes(numQuizzes);promptForInteger("Enter the number of quizzes: ",GradeBook.MIN_QUIZZES, GradeBook.MAX_QUIZZES);
-  
+        GradeBook2 studentNum = new GradeBook2();
+        studentNum.setStudents(promptForInteger("Enter the number of students: ",GradeBook2.MIN_STUDENTS, GradeBook2.MAX_STUDENTS));
+        GradeBook2 quizNum = new GradeBook2();
+        quizNum.setQuizzes(promptForInteger("Enter the number of quizzes: ",GradeBook2.MIN_QUIZZES, GradeBook2.MAX_QUIZZES));
         System.out.println();
-        System.out.println("Input the grades: ");
-//        final int NUM_STUDENTS = numStudents;
-//        final int NUM_QUIZZES =numQuizzes;
+        System.out.println("Input the Grades:");
+        GradeBook2 scoreNum = new GradeBook2();
         
-        System.out.println("The numbers are "+ numStudent.getStudents());
-        
-       
-        
-       
-        
-        
-        
-//        for (int numStudent=0; numStudent<NUM_STUDENTS; numStudent++)
-//        {
-//            for ( numQuizzes=0; numQuizzes<NUM_QUIZZES; numQuizzes++)
-//            {
-//               GradeBook score1 = new GradeBook();
-//               score1.inputGrade(numStudent, numQuizzes, scores);promptForDouble("Enter the score for Student#",GradeBook.MIN_SCORE, GradeBook.MAX_SCORE);
-//               
-//                
-//            }
-//            
-//            
-//           
-//        }
-        
-        
-  
-        
-   
-    }
-    
-//    method to prompt for integer and input validation 
-    public static int promptForInteger(String message, int minValue, int maxValue)
-    {
-        int students=0;
-        boolean done = false;
-        Scanner keyboard = new Scanner(System.in);
-        
-        do
+        double [][] scores =new double[studentNum.getStudents()][quizNum.getQuizzes()];
+//        created a for loop for the score input and score input validation 
+        for (int student=0; student<studentNum.getStudents(); student++)
         {
-            System.out.print(message);
-            students = Integer.parseInt(keyboard.nextLine());
+            for (int quiz=0; quiz<quizNum.getQuizzes(); quiz++)
+            {
+                GradeBook2 r = new GradeBook2();
+                System.out.printf("Enter the score for Student#%d and Quiz#%d: ",student,quiz);
+                double score = promptForDouble("",GradeBook2.MIN_SCORE,GradeBook2.MAX_SCORE)+1;
+                r.inputGrade(student, quiz, score);
+            }
             
-            if (students < minValue || students > maxValue)
+        }
+//        Used a the getters to complete printf line with corresponding numbers
+        GradeBook2 average = new GradeBook2();
+        System.out.printf("The average score for %d students and %d quizzes is %f",studentNum.getStudents(),quizNum.getQuizzes(),average.calcAvgScore());
+        
+        
+        
+        
+        
+        
+        
+    }
+//    loops used for the input validation message
+    public static int promptForInteger(String message, int minValue, int maxValue)
+        {
+            int students=0;
+            boolean done = false;
+            Scanner keyboard = new Scanner(System.in);
+            
+            do
+            {
+                System.out.print(message);
+                students = Integer.parseInt(keyboard.nextLine());
+                
+                if (students < minValue || students > maxValue)
             {
                 System.err.printf("Invalid Value.   The acceptable range is between %d and %d.  Please try again\n", minValue, maxValue);
             }
@@ -78,19 +65,22 @@
         } while (!done);
         
         return students;
-    }
-//    method to prompt for double and imput validation 
+       
+            
+        }
     public static double promptForDouble(String message, double minValue, double maxValue)
     {
-        double scores = 0;
-        boolean done = false;
-        Scanner keyboard = new Scanner(System.in);
-        
-        do
         {
-            System.out.print(message);
-            scores = Double.parseDouble(keyboard.nextLine());
-            if (scores < minValue || scores > maxValue)
+            double score=0;
+            boolean done = false;
+            Scanner keyboard = new Scanner(System.in);
+            
+            do
+            {
+                System.out.print(message);
+                score = Double.parseDouble(keyboard.nextLine());
+                
+                if (score < minValue || score > maxValue)
             {
                 System.err.printf("Invalid Value.   The acceptable range is between %f and %f.  Please try again\n", minValue, maxValue);
             }
@@ -101,16 +91,9 @@
             
         } while (!done);
         
-        return scores;
+        return score;
        
+            
+        }  
     }
-
-    
-    
-}
-
-
-
-
-
-
+    }
